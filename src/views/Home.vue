78201dd518bee2365d3959e512b9e5ae60e85210
Polovkin <zimboroda@gmail.com>
2020-12-23 00:15:34 +0200

    FKNG TABLETS

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -17,25 +17,29 @@ export default {
   position: relative;
   background-color: red;
   color: #fff;
-  &:after {
-    @include breakpoint($width__desktop-hd-min) {
-   content: 'max width: #{$max-width__desktop-hd-min} width togge: #{$width__desktop-hd-min}';
 
+  &:after {
+    @include breakpoint($width__desktop-hd) {
+      content: '4k';
     }
-    @include breakpoint($width__desktop-min) {
-      content: 'max width: #{$max-width__desktop-min}  width togge: #{$width__desktop-min}';
+    @include breakpoint($width__desktop-hd-min) {
+      content: 'max width: #{$max-width__desktop-hd-min} width togge: #{$width__desktop-hd-min}';
     }
-    @include breakpoint($width__tablet-land) {
-      content: 'max width: #{$max-width__tablet-land}  width togge: #{$width__tablet-land}';
-    }
-    @include breakpoint($width__tablet-port) {
-      content: 'max width: #{$max-width__tablet-port}  width togge: #{$width__tablet-port}';
-    }
-    @include breakpoint($width__mobile-max) {
-      content: 'max width: #{$max-width__mobile-max}  width togge: #{$width__mobile-max}';
-    }
+    //@include breakpoint($width__laptop) {
+    //  content: 'max width: #{$max-width__desktop-min}  width togge: #{$width__laptop}';
+    //}
+    //@include breakpoint($width__tablet-land) {
+    //  content: 'max width: #{$max-width__tablet-land}  width togge: #{$width__tablet-land}';
+    //}
+    //@include breakpoint($width__tablet-port) {
+    //  content: 'max width: #{$max-width__tablet-port}  width togge: #{$width__tablet-port}';
+    //}
+    //@include breakpoint($width__mobile-max) {
+    //  content: 'max width: #{$max-width__mobile-max}  width togge: #{$width__mobile-max}';
+    //}
   }
 }
+
 .elem {
   position: relative;
 
@@ -43,14 +47,15 @@ export default {
   &:after {
     color: $color__light;
     font-weight: bold;
-    padding: 20px;
+    padding: 60px;
     display: block;
     text-transform: uppercase;
     background-color: red;
-    @include breakpoint($desktop__all) {
-      content: 'desktop__all #{$desktop__all}';
-      background-color: rebeccapurple;
-    }
+    font-size: 40px;
+    //@include breakpoint($desktop__all) {
+    //  content: 'desktop__all #{$desktop__all}';
+    //  background-color: rebeccapurple;
+    //}
     @include breakpoint($desktop__laptop) {
       content: 'desktop__laptop #{$desktop__laptop}';
       background-color: aqua;
@@ -59,10 +64,11 @@ export default {
       content: 'desktop__laptop-min #{$desktop__laptop-min}';
       background-color: tomato;
     }
-    //@include breakpoint($tablet__all) {
-    //  content: 'tablet__all #{$tablet__all}';
-    //  background-color: saddlebrown;
-    //}
+    @include breakpoint($tablet__all) {
+      content: 'tablet__all #{$tablet__all}';
+      background-color: saddlebrown;
+    }
+
     @include breakpoint($tablet__landscape) {
       content: 'tablet__landscape #{$tablet__landscape}';
       background-color: blue;
@@ -75,10 +81,10 @@ export default {
       content: 'phone__all #{$phone__all}';
       background-color: sandybrown;
     }
-    @include breakpoint($phone__landscape) {
-      content: '$phone__landscape #{$phone__landscape}';
-      background-color: khaki;
-    }
+    //@include breakpoint($phone__landscape) {
+    //  content: '$phone__landscape #{$phone__landscape}';
+    //  background-color: khaki;
+    //}
   }
 
 }
